886673bf047f1b3d2effe776f4eb8834b5df215d
LordSir101 <tywunderlich15@gmail.com>
2024-11-04 15:51:16 -0500

    added enemy spawner

--- Assets/SpawnEnemy.cs
+++ Assets/SpawnEnemy.cs
@@ -1,13 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int numInitialEmemies = 5;
+    [SerializeField] private GameObject ringEnemyPrefab;
     void Start()
     {
-        
+        SpawnEnemies(numInitialEmemies);
+    }
+
+    private void SpawnEnemies(int num)
+    {
+        for(int i = 0; i < num; i++)
+        {
+            float xPos = UnityEngine.Random.Range(0, 11);
+            float yPos = UnityEngine.Random.Range(0, 11);
+
+            Instantiate(ringEnemyPrefab, new Vector3(xPos, yPos, 0), transform.rotation);
+        }
     }
 
     // Update is called once per frame
