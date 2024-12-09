b93027341a07d4b719b92b4fc5d36ce4f6e1d76c
LordSir101 <tywunderlich15@gmail.com>
2024-12-09 13:53:08 -0500

    increased healing shrine heal amount

--- Assets/Shrines/HealingShrine.cs
+++ Assets/Shrines/HealingShrine.cs
@@ -18,7 +18,7 @@ public class HealingShrine : Shrine
     protected override void ShowUpgradeText()
     {
         interactText.enabled = true;
-        interactText.text = $"Heal 30% health ({upgradeCosts[numUpgrades]})";
+        interactText.text = $"Heal 50% health ({upgradeCosts[numUpgrades]})";
     }
 
     protected override void Upgrade(InputAction.CallbackContext context)
@@ -28,7 +28,7 @@ public class HealingShrine : Shrine
         if(rm.Essence >= upgradeCosts[numUpgrades])
         {
             rm.Essence -= upgradeCosts[numUpgrades];
-            playerHealthScript.HealPercentHealthOverTime(0.3f, 10);
+            playerHealthScript.HealPercentHealthOverTime(0.5f, 10);
             numUpgrades++;
 
             LightCandle();
